6762519cd58b5584802d374e927041cd46e7fb3d
YounGTeX <thiagoextremer@gmail.com>
2022-10-12 20:08:53 -0300

    F: apenas a parte do regex, falta booyermoore

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import tarfile
 import re
+import time
 
 tar = tarfile.open('./enron_mail_20150507.tar.gz', mode='r:gz')
 
 regexDollar = re.compile('(\$[0-9]+([.,][0-9]+)?\s?((B|[Bb]illions?)?|(MM|[Mm]illions?)?|([Kk]|M)?)?\s)')
 regexEmail = re.compile('(([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+)')
 regexUrl = re.compile('((http://|https://)?(www.)([a-zA-Z])+.[a-z]+(.[a-z]+)+)')
+regexPrice = re.compile('(price)')
 
 maxValue = 0
 maxValueStr = ''
@@ -14,8 +16,10 @@ maxValueMember = None
 maxEmails = 0
 maxEmailsMember = None
 emailNumber = 0
+priceNumber = 0
 
 addrAmount = {}
+# inicio = time.time()
 for member in tar.getmembers():
     file = tar.extractfile(member)
     if file:
@@ -24,7 +28,7 @@ for member in tar.getmembers():
         matchEmailNumber = regexEmail.findall(str(f.strip(), 'utf-8'))
         for match in matchEmailNumber:
             print(match[0])
-        emailNumber = len(matchEmailNumber)
+        emailNumber += len(matchEmailNumber)
         #Devido aos emails possuírem a terminação ".com", é impossível saber o país de residência do email.
         #Portanto, optamos por fazer uma contagem total dos emails.
 
@@ -56,8 +60,17 @@ for member in tar.getmembers():
                 addrAmount[match[0]] +=1
             else:
                 addrAmount[match[0]] = 1
-    
+
+        #f
+        matchPrice = regexPrice.findall(str(f.strip()))
+        for match in matchPrice:
+            print("Achou!")
+        priceNumber += 1
+        
+# fim = time.time()
+# tempoPrice = (fim - inicio)
 sortedAddrAmount = {k: v for k, v in sorted(addrAmount.items(), key=lambda item: item[1], reverse=True)}
+
         
 # a)
 print(f"Número de emails: {emailNumber}")
@@ -76,3 +89,10 @@ print(f"""URLs mais frequentes:
 {list(sortedAddrAmount.keys())[3]}: {list(sortedAddrAmount.values())[3]} correspondências
 {list(sortedAddrAmount.keys())[4]}: {list(sortedAddrAmount.values())[4]} correspondências
 """)
+
+#f)
+print(f"Price(s) encontrados(s): {priceNumber}")
+# print(f"Tempo total do regex: {tempoPrice}")
+#durante o teste rodando apenas o regex do price os resultados obtidos foram:
+# Price(s) encontrados(s): 517401
+# Tempo total do regex: 58.482699155807495
